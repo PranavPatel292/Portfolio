c65c4c101b3392928f8d43cd07eae3252cad0481
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-07-25 20:59:13 +1000

    final 3

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
-import { Route, BrowserRouter as Routes  } from 'react-router-dom'
+import { HashRouter, Route, BrowserRouter as Routes  } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './AboutMe'
@@ -9,6 +9,7 @@ import ContactMe from './ContactMe'
 
 function Header(){
 	return(
+    <HashRouter basepath="/">
     <div>
     <div class="image">
     <Routes>
@@ -16,22 +17,23 @@ function Header(){
         <Navbar bg="light" expand="lg">
           <Navbar.Collapse id="basic-navbar-nav">
              <Nav className="mr-auto">
-              <Nav.Link href={process.env.PUBLIC_URL + "/AboutMe"} default>About Me</Nav.Link>
-              <Nav.Link href={process.env.PUBLIC_URL + "/Project"}>Project</Nav.Link>
-              <Nav.Link href={process.env.PUBLIC_URL + "/ContactMe"}>Contact Me</Nav.Link>
-              <Nav.Link href={process.env.PUBLIC_URL + "#"}>Contact Me 1</Nav.Link>
+              <Nav.Link href="/AboutMe" default>About Me</Nav.Link>
+              <Nav.Link href="/Project">Project</Nav.Link>
+              <Nav.Link href="/ContactMe">Contact Me</Nav.Link>
+              <Nav.Link href="#">Contact Me 2</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             </Navbar>
-            <Route path={process.env.PUBLIC_URL + "/Project"} component={Project}/>
-            <Route path={process.env.PUBLIC_URL + "/AboutMe"} component={AboutMe} />
-            <Route path={process.env.PUBLIC_URL + "/ContactMe"} component={ContactMe}/>
-            <Route exact path={"/"} component={AboutMe}/>
+            <Route path="/Project" component={Project}/>
+            <Route path="/AboutMe" component={AboutMe} />
+            <Route path="/ContactMe" component={ContactMe}/>
+            <Route exact path="/" component={AboutMe}/>
 
       </div>
     </Routes> 
     </div>
     </div>
+    </HashRouter>
 	)
 }
 
