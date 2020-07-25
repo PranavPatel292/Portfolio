9092131155afc1bb42809559f272c6d21cf572ec
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-07-25 23:10:21 +1000

    final 10

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
-import { Route, BrowserRouter as Routes  } from 'react-router-dom'
+import { Route, Link, HashRouter as Routes  } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './AboutMe'
@@ -11,15 +11,15 @@ function Header(){
 	return(
     <div>
     <div class="image">
-    <Routes basename={process.env.PUBLIC_URL}>
+    <Routes>
     {console.log("hii", process.env.PUBLIC_URL)}
       <div>
         <Navbar bg="light" expand="lg">
           <Navbar.Collapse id="basic-navbar-nav">
              <Nav className="mr-auto">
-              <Nav.Link href="/AboutMe" default>About Me</Nav.Link>
-              <Nav.Link href="/Project">Project</Nav.Link>
-              <Nav.Link href="/ContactMe">Contact Me</Nav.Link>
+              <Nav.Link as={Link} to="/AboutMe" default>About Me</Nav.Link>
+              <Nav.Link as={Link} to="/Project">Project</Nav.Link>
+              <Nav.Link as={Link} to="/ContactMe">Contact Me</Nav.Link>
               <Nav.Link href="#">Contact Me 1</Nav.Link>
             </Nav>
             </Navbar.Collapse>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDom from 'react-dom'
 import App from './App'
+import { HashRouter as Router } from 'react-router-dom'
 
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDom.render(<Router>< App /></Router>, document.getElementById('root'));
