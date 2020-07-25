5161e8e8afbf11536ad01fa3ffda63ec670e9571
Pranav Patel <42827820+PranavPatel292@users.noreply.github.com>
2020-07-25 20:37:04 +1000

    test commit 4

--- src/Header.js
+++ src/Header.js
@@ -11,8 +11,24 @@ function Header(){
 	return(
     <div>
     <div class="image">
-      <h1 style={{color: "white"}}> hii </h1>
-      <h1 style={{color: "orange"}}> hello </h1>
+    <Routes>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Collapse id="basic-navbar-nav">
+             <Nav className="mr-auto">
+              <Nav.Link href="/AboutMe" default>About Me</Nav.Link>
+              <Nav.Link href="/Project">Project</Nav.Link>
+              <Nav.Link href="/ContactMe">Contact Me</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+            <Route path="/Project" component={Project}/>
+            <Route path="/AboutMe" component={AboutMe} />
+            <Route path="/ContactMe" component={ContactMe}/>
+            <Route exact path="/" component={AboutMe}/>
+
+      </div>
+    </Routes> 
     </div>
     </div>
 	)
